866ccf7d04c43e52bb4e20b9f5a9659f52baa4fb
CK2740 <chaitanyakaldante27@gmail.com>
2026-07-19 20:19:45 +0530

    changes in upload.js

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,21 +1,11 @@
 import express from "express";
 import multer from "multer";
-import fs from "fs";
 import pdfParse from "pdf-parse";
 import { analyzeResume } from "../utils/groq.js";
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 router.post("/upload-resume", upload.single("resume"), async (req, res) => {
@@ -29,15 +19,13 @@ router.post("/upload-resume", upload.single("resume"), async (req, res) => {
             });
         }
 
-        const dataBuffer = fs.readFileSync(req.file.path);
-
-        const pdfData = await pdfParse(dataBuffer);
+        const pdfData = await pdfParse(req.file.buffer);
         const analysis = await analyzeResume(pdfData.text);
 
        res.json({
         success: true,
         message: "Resume analyzed successfully!",
-        file: req.file.filename,
+        file: req.file.originalname,
         analysis: analysis
        });
 
